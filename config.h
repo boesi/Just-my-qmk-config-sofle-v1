935dc5c852dd79751a8d9f4d2f9d683c9d095abe
Alexander Bösecke <boesi@users.noreply.github.com>
2025-07-12 11:22:15 +0200

    initial commit

---
+++ config.h
@@ -0,0 +1,16 @@
+#pragma once
+
+/* The way how "handedness" is decided (which half is which),
+see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
+for more options.
+*/
+/* #define IGNORE_MOD_TAP_INTERRUPT */
+#undef  TAPPING_TERM
+#define TAPPING_TERM 200
+#undef  TAPPING_TOGGLE
+#define TAPPING_TOGGLE 3
+#define WPM_SAMPLE_PERIODS 50
+/* #define PERMISSIVE_HOLD_PER_KEY */
+#define UNICODE_SELECTED_MODES UNICODE_MODE_LINUX, UNICODE_MODE_WINCOMPOSE
+#define SPLIT_USB_DETECT
+#define DEBOUNCE 10
